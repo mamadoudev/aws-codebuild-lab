691cc3b89f57523a9b127fe6621f25f75986a16d
Mamadou DEMBELE <mamadoudev@gmail.com>
2026-08-14 20:58:13 -0400

    Test volontaire corrige  CodeBuild

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 def additionner(nombre1: int, nombre2: int) -> int:
     return nombre1 + nombre2
 
+def carre(nombre1: int, nombre2: int) -> int:
+    return nombre1 + nombre2
+
 
 if __name__ == "__main__":
     resultat = additionner(5, 3)
